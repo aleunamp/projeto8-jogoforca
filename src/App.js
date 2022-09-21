1f41e3b94f3c8cc33f6dd97ed342838823a16d5a
aleunamp <manuella.m.p182@gmail.com>
2022-09-21 17:27:08 -0300

    versao com importacoes, alfabeto e palavras

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import palavras from "./Palavras";
+import forca0 from "./assets/forca0.png";
+import forca1 from "./assets/forca1.png";
+import forca2 from "./assets/forca2.png";
+import forca3 from "./assets/forca3.png";
+import forca4 from "./assets/forca4.png";
+import forca5 from "./assets/forca5.png";
+import forca6 from "./assets/forca6.png";
+
+
+export default function App() {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    console.log(alfabeto);
+
+    console.log(palavras);
+
+    const imagensDaForca = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
+    console.log(imagensDaForca);
+
+    return "oiee";
+}
